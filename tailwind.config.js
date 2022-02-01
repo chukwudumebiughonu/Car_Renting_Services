fdf6aa4f6fc261f3545beeb7abbcb1e7ed37df87
chukwudumebiughonu <plasdexcel14@gmail.com>
2022-02-01 10:00:12 +0100

    new host

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-  purge: ['./src/**/*.{js, jsx, ts, tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
+  // purge: ['./src/**/*.{js, jsx, ts, tsx}', './public/index.html'],
+  content: ['./src/**/*.{js, jsx, ts, tsx}', './public/index.html'],
+  darkMode: 'media',  // false or 'media' or 'class'
   theme: {
     extend: {},
     screens: {
